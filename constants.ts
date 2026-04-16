96b6c3c70fc155359b1ff6bdbae3c80c8a66b55f
journalthatjourney <hello@journalthatjourney.com>
2026-04-16 16:23:36 -0500

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -72,9 +72,7 @@ export const PRODUCTS: Product[] = [{
     price: '$12.00',
     imageSrc: 'https://i.postimg.cc/CLZKJ3wv/daily_pause.png',
     imageAlt:'Your Daily Pause: A Journal for Intentional Living',                                     
-   },
-  {   
-  },
+   }    
 ];
 export const SOCIAL_LINKS = {
   email: 'hello@journalthatjourney.com',
