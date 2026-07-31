37c334241c63ba9d437a50da8d08705d27d61757
journalthatjourney <hello@journalthatjourney.com>
2026-07-30 20:01:15 -0500

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { Navbar } from './components/Navbar';
 import Hero from './components/Hero';
 import ProductGrid from './components/ProductGrid';
 import About from './components/About';
+import Challenge from './components/Challenge';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 
@@ -11,11 +12,12 @@ const App: React.FC = () => {
     <div className="min-h-screen bg-brand-dark overflow-x-hidden">
       <Navbar />
       <main>
-        <Hero />
-        <ProductGrid />
-        <About />
-        <Contact />
-      </main>
+  <Hero />
+  <ProductGrid />
+  <About />
+  <Challenge />
+  <Contact />
+</main>
       <Footer />
     </div>
   );
